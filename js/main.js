c8ddf20716382a359ecd7c30bb63be0d756500e1
Andrew Faircloth <andrewkfaircloth@gmail.com>
2015-10-08 13:36:18 -0400

    content in first box

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 (function () {
 
-  
   var templateString = $('#itemTemplate').html();
 
   var templateFunction = _.template(templateString);
+
+  var firstTwentyFive = _.first(etsy.results,25);
+
+  console.log(firstTwentyFive);
 
   _.each(etsy.results, function (item) {
     console.log(item);
